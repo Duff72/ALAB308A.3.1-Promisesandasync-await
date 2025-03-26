757ea4ad0f03c7a5ab426498f6c6a53ec5f7100c
Duff72 <duffy.andd@gmail.com>
2025-03-26 13:30:10 -0400

    completed assignment

--- promises.mjs
+++ promises.mjs
@@ -1,10 +1,26 @@
 // Importing database functions. DO NOT MODIFY THIS LINE.
-import { central, db1, db2, db3, vault } from "./databases.js";
+import { central, db1, db2, db3, vault } from "./databases.mjs";
+let test = [];
 
-function getUserData(id) {
+async function getUserData(id) {
+  let result = [];
   const dbs = {
     db1: db1,
     db2: db2,
     db3: db3,
   };
+  try {
+    const database = await central(id);
+    const userInfo1 = await dbs[database](id);
+    const userInfo2 = await vault(id);
+    const userData = {
+      ...userInfo1,
+      ...userInfo2,
+    };
+    return console.log(userData);
+  } catch {
+    console.log("error getting user data");
+  }
 }
+
+getUserData(1);
